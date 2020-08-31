882f6029f51835b06f117ca90e4b3fd7b7de65ca
TCsTheMechanic <ceruttioliveira@hotmail.com>
2020-08-31 17:46:31 -0300

    Create match card

---
+++ src/components/Match-card/Match-card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className='match-card'>
+      <h2>Match Card</h2>
+    </div>
+  )
+}
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Header from '../../components/Header/Header'
 import GameMenu from '../../components/Game-menu/Game-menu'
+import MatchCard from '../../components/Match-card/Match-card'
 
 export default () => {
   return (
     <div id='landing-page'>
       <Header />
       <GameMenu />
+      <MatchCard />
     </div>
   )
 }
