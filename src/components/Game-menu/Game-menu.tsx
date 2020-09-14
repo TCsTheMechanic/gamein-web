a96d4492a031f51148395296f9f70a0173385cc0
TCsTheMechanic <ceruttioliveira@hotmail.com>
2020-09-14 14:07:25 -0300

    Remove swiper

--- src/components/Game-menu/Game-menu.tsx
+++ src/components/Game-menu/Game-menu.tsx
@@ -1,6 +1,6 @@
 import './Game-menu.css'
 
-import React from 'react'
+import React  from 'react'
 
 import GameButton from '../Game-button/Game-button'
 
@@ -13,17 +13,18 @@ import valorantIcon from '../../assets/images/game-cards/valorant.png'
 import apexIcon from '../../assets/images/game-cards/apex-legends.png'
 
 export default () => {
+
   return (
-    <div className='game-menu'>
-      <div className='menu-items'>
-        <GameButton image={csIcon} description='CS GO' />
-        <GameButton image={fortniteIcon} description='Fortnite' />
-        <GameButton image={lolIcon} description='League of Legends' />
-        <GameButton image={hsIcon} description='Heart Stone' />
-        <GameButton image={r6Icon} description='Rainbow Six Siege' />
-        <GameButton image={valorantIcon} description='Valorant' />
-        <GameButton image={apexIcon} description='Apex Legends' />
-      </div>
+  <div className='game-menu'>
+    <div className='menu-items'>
+      <GameButton image={csIcon} description='CS GO' />
+      <GameButton image={fortniteIcon} description='Fortnite' />
+      <GameButton image={lolIcon} description='League of Legends' />
+      <GameButton image={hsIcon} description='Heart Stone' />
+      <GameButton image={r6Icon} description='Rainbow Six Siege' />
+      <GameButton image={valorantIcon} description='Valorant' />
+      <GameButton image={apexIcon} description='Apex Legends' />
     </div>
+  </div>
   )
 }
