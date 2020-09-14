a6c88256c5283d3138c2601196bb465231969955
TCsTheMechanic <ceruttioliveira@hotmail.com>
2020-09-14 15:16:33 -0300

    Finish header sidebar menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,48 @@
 import './Header.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import logo from '../../assets/images/icon.png'
+import { GoThreeBars } from 'react-icons/go'
+import { CgClose } from 'react-icons/cg'
 
 export default () => {
+  const [sideBar, setSideBar] = useState(false)
+
+  const showSideBar = () => setSideBar(!sideBar)
+
   return (
     <header className='page-header'>
       <div className='top-bar-container'>
-        <div className='top-bar-menu-left'>
+        <div className='top-bar-left'>
+          <Link to='#' className='side-bar-icon' onClick={showSideBar}>
+            {sideBar ? <CgClose/> : <GoThreeBars/>}
+          </Link>
+          <div className={sideBar ? 'side-bar-menu active' : 'side-bar-menu'}>
+            <ul>
+              <li>
+                <Link id='link' className='top-bar-menu-item animated-text' to='/teams'>
+                  TEAMS
+                </Link>
+              </li>
+              <li>
+                <Link id='link' className='top-bar-menu-item animated-text' to='/calendar'>
+                  CALENDAR
+                </Link>
+              </li>
+              <li>
+                <Link id='link' className='top-bar-menu-item animated-text' to='/watch'>
+                  WATCH
+                </Link>
+              </li>
+              <li>
+                <Link id='link' className='top-bar-menu-item animated-text' to='/about'>
+                  ABOUT US
+                </Link>
+              </li>
+            </ul>
+          </div>
           <div className='logo-container'>
             <Link className='logo-container-img' to='/'>
               <img src={logo} alt="Go back" />
@@ -19,7 +52,7 @@ export default () => {
               Gaming Statistics
             </Link>
           </div>
-          <div className='top-bar-menu-items'>
+          <div className='top-bar-menu'>
             <Link id='link' className='top-bar-menu-item animated-text' to='/teams'>
               TEAMS
             </Link>
@@ -31,10 +64,10 @@ export default () => {
             </Link>
             <Link id='link' className='top-bar-menu-item animated-text' to='/about'>
               ABOUT US
-          </Link>
+            </Link>
           </div>
         </div>
-        <div className='top-bar-menu-right'>
+        <div className='top-bar-login-menu'>
           <Link id='link' to='/login'>
             <button>Login</button>
           </Link>
