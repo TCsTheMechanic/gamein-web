0e37678ecdd34ac57adc76d901ba577bc0bbb044
TCsTheMechanic <ceruttioliveira@hotmail.com>
2020-10-15 12:19:28 -0300

    Add games pages routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -9,7 +9,35 @@ export default () => {
     <BrowserRouter>
       <Route
         exact path='/'
+        render={() => <Landing background={''} />}
+      />
+      <Route
+        exact path='/CS GO'
         render={() => <Landing background={LandingBackgrounds[0].img} />}
+      />
+      <Route
+        exact path='/Fortnite'
+        render={() => <Landing background={LandingBackgrounds[1].img} />}
+      />
+      <Route
+        exact path='/League Of Legends'
+        render={() => <Landing background={LandingBackgrounds[2].img} />}
+      />
+      <Route
+        exact path='/Hearthstone'
+        render={() => <Landing background={LandingBackgrounds[3].img} />}
+      />
+      <Route
+        exact path='/Rainbow Six Siege'
+        render={() => <Landing background={LandingBackgrounds[4].img} />}
+      />
+      <Route
+        exact path='/Valorant'
+        render={() => <Landing background={LandingBackgrounds[5].img} />}
+      />
+      <Route
+        exact path='/Apex Legends'
+        render={() => <Landing background={LandingBackgrounds[6].img} />}
       />
     </BrowserRouter>
   )
